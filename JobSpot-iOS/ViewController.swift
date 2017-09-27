61156c3aaf32cb5e11812052844e1dc2334579d0
Marlow Fernandez <marlow@fullsail.edu>
2017-09-26 20:33:34 -0400

    Init Commit
    Initial Commit

---
+++ JobSpot-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JobSpot-iOS
+//
+//  Created by Krista Appel and Marlow Fernandez on 9/26/17.
+//  Copyright © 2017 JobSpot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
