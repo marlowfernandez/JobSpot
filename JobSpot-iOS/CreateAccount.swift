6efb0c2ba292c09801472379083cf782f821c40c
Marlow Fernandez <marlow@fullsail.edu>
2017-09-29 18:20:12 -0400

    Account login and create working. Home Page created, Profile page created

--- JobSpot-iOS/CreateAccount.swift
+++ JobSpot-iOS/CreateAccount.swift
@@ -9,61 +9,62 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class CreateAccount: UIViewController {
 
     @IBOutlet weak var emailAddress: UITextField!
     @IBOutlet weak var inputPassword: UITextField!
-    let mainToCreateAcc = "mainToCreateAcc"
-    let mainToHome = "mainToHome"
     var handle: FIRAuthStateDidChangeListenerHandle?
+    let createAccToHome = "createAccToHome"
+    let createAccToLogin = "createAccToLogin"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        debugPrint("viewDidLoad")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        debugPrint("didReceiveMemoryWarning")
         // Dispose of any resources that can be recreated.
     }
-
-    @IBAction func buttonLogin(_ sender: UIButton) {
-        debugPrint("button login pressed")
+    
+    @IBAction func registerButtonAction(_ sender: Any) {
         
         let email = emailAddress.text
         let password = inputPassword.text
         
         if email != "" && password != "" {
-            FIRAuth.auth()?.signIn(withEmail: email!, password: password!) { (user, error) in
-                if error != nil {
-                    debugPrint("not able to login")
+            
+            FIRAuth.auth()?.createUser(withEmail: email!, password: password!) { (user, error) in
+                if let error = error {
                     
-                    let loginError = UIAlertController(title: "Login Error", message: "Error logging in: "+(error?.localizedDescription)!+" Do you have an account? Please register", preferredStyle: UIAlertControllerStyle.alert)
-                    loginError.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
-                    self.present(loginError, animated: true, completion: nil)
+                    let createError = UIAlertController(title: "Account Creation Error", message: "\(error.localizedDescription) Please try again with a different email.", preferredStyle: UIAlertControllerStyle.alert)
+                    createError.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                    self.present(createError, animated: true, completion: nil)
                     
                     return
                 }
-                debugPrint("able to login")
-                //self.performSegue(withIdentifier: self.mainToHome, sender: nil)
+                debugPrint("\(user!.email!) was created")
             }
-            
             
             debugPrint(email!,password!)
             emailAddress.text = ""
             inputPassword.text = ""
         } else {
+            
             let emptyFields = UIAlertController(title: "Error", message: "Enter text into email and/or password fields", preferredStyle: UIAlertControllerStyle.alert)
             emptyFields.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
             self.present(emptyFields, animated: true, completion: nil)
+            
+            debugPrint("registerButton not successful")
         }
-        
     }
     
     
-    @IBAction func registerButtonAction(_ sender: Any) {
-        self.performSegue(withIdentifier: self.mainToCreateAcc, sender: nil)
+    @IBAction func actionLogin(_ sender: Any) {
+        self.performSegue(withIdentifier: self.createAccToLogin, sender: nil)
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -71,7 +72,7 @@ class ViewController: UIViewController {
         
         handle = FIRAuth.auth()?.addStateDidChangeListener() { (auth, user) in
             if user != nil {
-                self.performSegue(withIdentifier: self.mainToHome, sender: nil)
+                self.performSegue(withIdentifier: self.createAccToHome, sender: nil)
                 debugPrint(user?.email! as Any)
             }
         }
---
+++ JobSpot-iOS/HomeController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  JobSpot-iOS
+//
+//  Created by Krista Appel and Marlow Fernandez on 9/26/17.
+//  Copyright © 2017-2018 JobSpot. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class HomeController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        debugPrint("viewDidLoad")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        debugPrint("didReceiveMemoryWarning")
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
