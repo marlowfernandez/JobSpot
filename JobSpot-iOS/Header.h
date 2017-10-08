59ff527294948ce3d0dcaa1be2c5d57a18c0f123
Marlow Fernandez <marlow@fullsail.edu>
2017-10-08 15:01:17 -0400

    setting up LinkedIn SDK with files and bridge header

---
+++ JobSpot-iOS/Header.h
@@ -0,0 +1,16 @@
+//
+//  Header.h
+//  JobSpot-iOS
+//
+//  Created by Marlow Fernandez on 10/8/17.
+//  Copyright © 2017 JobSpot. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+#import <linkedin-sdk/LISDK.h>
+
+#endif /* Header_h */
