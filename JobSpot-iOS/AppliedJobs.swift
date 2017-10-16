549b9bf9e9fab37407bb6171f31d85b6ca314495
Marlow Fernandez <marlow@fullsail.edu>
2017-10-15 22:29:36 -0400

    applied jobs are viewable

--- JobSpot-iOS/AppliedJobs.swift
+++ JobSpot-iOS/AppliedJobs.swift
@@ -39,32 +39,62 @@ class AppliedJobs: UIViewController, UITableViewDataSource, UITableViewDelegate 
         bannerViewOutlet.load(request)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        rootRef = FIRDatabase.database().reference()
+        
+        let userID = FIRAuth.auth()?.currentUser?.uid
+        
+        //print("userID: \(String(describing: userID))")
+        
+        let usersRef = rootRef.child("users")
+        
+        //print("usersRef: \(usersRef)")
+        
+        let idRef = usersRef.child(userID!)
+        
+        //print("ifRef: \(idRef)")
+        
+        let listRef = idRef.child("appliedjobs")
+        
+        debugPrint("listRef: \(listRef)")
+        
+        listRef.observe(.value, with: { snapshot in
+            self.appliedJobsItem.removeAll()
+            for item in snapshot.children {
+                let structItem = SaveJob(snapshot: item as! FIRDataSnapshot)
+                print("structItem: \(structItem)")
+                self.appliedJobsItem.append(structItem)
+            }
+            self.tableViewOutlet.reloadData()
+            print("savedjobsItems List: \(self.appliedJobsItem)")
+        })
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return noItems.count
-//        if appliedJobsItem.count > 0 {
-//            return appliedJobsItem.count
-//        } else {
-//            return noItems.count
-//        }
+        if appliedJobsItem.count > 0 {
+            return appliedJobsItem.count
+        } else {
+            return noItems.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCellAppliedJobs", for: indexPath)
         cell.selectionStyle = .none
         
-        let noData = noItems[indexPath.row]
-        cell.textLabel?.text = noData
-        cell.detailTextLabel?.text = " "
-        
-        //        if jobItems.count > 0 {
-        //            let jobData = jobItems[indexPath.row]
-        //            cell.textLabel?.text = jobData.companyName
-        //            cell.detailTextLabel?.text = jobData.jobTitle
-        //        } else {
-        //            let noData = noItems[indexPath.row]
-        //            cell.textLabel?.text = noData
-        //            cell.detailTextLabel?.text = " "
-        //        }
+        if appliedJobsItem.count > 0 {
+            let jobData = appliedJobsItem[indexPath.row]
+            cell.textLabel?.text = jobData.jobTitleGlobal
+            cell.detailTextLabel?.text = jobData.companyNameGlobal
+        } else {
+            let noData = noItems[indexPath.row]
+            cell.textLabel?.text = noData
+            cell.detailTextLabel?.text = " "
+        }
         
         return cell
         
@@ -72,82 +102,87 @@ class AppliedJobs: UIViewController, UITableViewDataSource, UITableViewDelegate 
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if (editingStyle == UITableViewCellEditingStyle.delete) {
-            //            let savedItem = savedSearchesItem[indexPath.row]
-            //            savedItem.ref?.removeValue()
-            //            self.tableViewOutlet.reloadData()
-            print("ok")
+            let alert = UIAlertController(title: "Delete", message: "Are you sure you want to delete this as an applied job of yours?", preferredStyle: .alert)
+            
+            let deleteAction = UIAlertAction(title: "Yes", style: .default) { (action) in
+                let savedItem = self.appliedJobsItem[indexPath.row]
+                savedItem.ref?.removeValue()
+                self.tableViewOutlet.reloadData()
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            
+            alert.addAction(deleteAction)
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true, completion: nil)
+            
         }
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let indexPath = tableView.indexPathForSelectedRow
-        //        let savedJobItem = savedSearchesItem[(indexPath?.row)!]
-        //
-        //        print("selected saved item: \(savedItem)")
-        //
-        //        let alertController = UIAlertController(title: "Saved Searches", message: "Would you like to use this search again?", preferredStyle: .alert)
-        //
-        //        alertController.addAction(UIAlertAction(title: "Search Again", style: .default, handler: {
-        //            alert -> Void in
-        //
-        //            SaveSearch.days = savedItem.days2
-        //            SaveSearch.keywords = savedItem.keyword2
-        //            SaveSearch.location = savedItem.location2
-        //            SaveSearch.radius = savedItem.radius2
-        //
-        //            print("SaveSearch days: \(SaveSearch.days)")
-        //            print("SaveSearch keywords: \(SaveSearch.keywords)")
-        //            print("SaveSearch location: \(SaveSearch.location)")
-        //            print("SaveSearch radius: \(SaveSearch.radius)")
-        //
-        //            self.performSegue(withIdentifier: self.savedsearchesToHome, sender: nil)
-        //        }))
-        //
-        //        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {
-        //            alert -> Void in
-        //
-        //        }))
-        //
-        //        alertController.addTextField(configurationHandler: { (textField) -> Void in
-        //            textField.text = "Days: " + savedItem.days2
-        //            textField.textAlignment = .left
-        //            textField.isUserInteractionEnabled = false
-        //        })
-        //
-        //        alertController.addTextField(configurationHandler: { (textField) -> Void in
-        //            textField.text = "Keyword: " + savedItem.keyword2
-        //            textField.textAlignment = .left
-        //            textField.isUserInteractionEnabled = false
-        //        })
-        //
-        //        alertController.addTextField(configurationHandler: { (textField) -> Void in
-        //            textField.text = "Location: " + savedItem.location2
-        //            textField.textAlignment = .left
-        //            textField.isUserInteractionEnabled = false
-        //        })
-        //        
-        //        alertController.addTextField(configurationHandler: { (textField) -> Void in
-        //            textField.text = "Radius: " + savedItem.radius2
-        //            textField.textAlignment = .left
-        //            textField.isUserInteractionEnabled = false
-        //        })
-        //        
-        //        self.present(alertController, animated: true, completion: nil)
-    }
-    
-    @IBAction func homeActionButton(_ sender: UIButton) {
-        self.performSegue(withIdentifier: self.appliedToHome, sender: nil)
-    }
-    
-    @IBAction func savedActionButton(_ sender: UIButton) {
-        self.performSegue(withIdentifier: self.appliedToSavedJobs, sender: nil)
-    }
-    
-    @IBAction func appliedActionButton(_ sender: UIButton) {
-    }
-    
-    @IBAction func profileActionButton(_ sender: UIButton) {
-        self.performSegue(withIdentifier: self.appliedToProfile, sender: nil)
+        
+        if appliedJobsItem.count > 0 {
+            let savedJobItem = appliedJobsItem[(indexPath?.row)!]
+            
+            print("selected saved item: \(savedJobItem)")
+            
+            let alertController = UIAlertController(title: "Applied Jobs", message: "Would you like to view this job?", preferredStyle: .alert)
+            
+            alertController.addAction(UIAlertAction(title: "View Job", style: .default, handler: {
+                alert -> Void in
+                
+                DisplayStruct.companyNameGlobal = savedJobItem.companyNameGlobal
+                DisplayStruct.datePostedGlobal = savedJobItem.datePostedGlobal
+                DisplayStruct.jobCityStateGlobal = savedJobItem.jobCityStateGlobal
+                DisplayStruct.jobIDGlobal = savedJobItem.jobIDGlobal
+                DisplayStruct.jobLatGlobal = savedJobItem.jobLatGlobal
+                DisplayStruct.jobLngGlobal = savedJobItem.jobLngGlobal
+                DisplayStruct.jobTitleGlobal = savedJobItem.jobTitleGlobal
+                DisplayStruct.jobURLGlobal = savedJobItem.jobURLGlobal
+                
+                print("SavedJobs companyNameGlobal: \(DisplayStruct.companyNameGlobal)")
+                print("SavedJobs datePostedGlobal: \(DisplayStruct.datePostedGlobal)")
+                print("SavedJobs jobCityStateGlobal: \(DisplayStruct.jobCityStateGlobal)")
+                print("SavedJobs jobIDGlobal: \(DisplayStruct.jobIDGlobal)")
+                print("SavedJobs jobLatGlobal: \(DisplayStruct.jobLatGlobal)")
+                print("SavedJobs jobLngGlobal: \(DisplayStruct.jobLngGlobal)")
+                print("SavedJobs jobTitleGlobal: \(DisplayStruct.jobTitleGlobal)")
+                print("SavedJobs jobURLGlobal: \(DisplayStruct.jobURLGlobal)")
+                
+                //self.performSegue(withIdentifier: self.savedjobsToDisplay, sender: nil)
+            }))
+            
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: {
+                alert -> Void in
+                
+            }))
+            
+            alertController.addTextField(configurationHandler: { (textField) -> Void in
+                textField.text = "Company Name: " + savedJobItem.companyNameGlobal
+                textField.textAlignment = .left
+                textField.isUserInteractionEnabled = false
+            })
+            
+            alertController.addTextField(configurationHandler: { (textField) -> Void in
+                textField.text = "Title: " + savedJobItem.jobTitleGlobal
+                textField.textAlignment = .left
+                textField.isUserInteractionEnabled = false
+            })
+            
+            alertController.addTextField(configurationHandler: { (textField) -> Void in
+                textField.text = "Location: " + savedJobItem.jobCityStateGlobal
+                textField.textAlignment = .left
+                textField.isUserInteractionEnabled = false
+            })
+            
+            alertController.addTextField(configurationHandler: { (textField) -> Void in
+                textField.text = "Date Posted: " + savedJobItem.datePostedGlobal
+                textField.textAlignment = .left
+                textField.isUserInteractionEnabled = false
+            })
+            
+            self.present(alertController, animated: true, completion: nil)
+        }
     }
     
 }
